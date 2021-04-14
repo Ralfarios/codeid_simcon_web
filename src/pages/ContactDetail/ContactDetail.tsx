037c717580f3d55096b14f2e6816da866a302f92
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-04-14 20:26:44 +0700

    Done

--- src/pages/ContactDetail/ContactDetail.tsx
+++ src/pages/ContactDetail/ContactDetail.tsx
@@ -1,18 +1,23 @@
+import ContactDetailContainer from './components/ContactDetailContainer';
+import ContactPhoneNumber from './components/ContactPhoneNumber';
+import MetaDecorator from '../../utils/helmet/MetaDecorator';
+
 import './styles/contactDetail.css';
 
 const ContactDetail = () => {
   return (
     <div id="ContactDetail">
-      <div className="contactDetailContainer">
-        <div className="contactDetailAvaContainer">
-          <img src="https://i.pinimg.com/originals/ea/b8/60/eab860a9c45b720fbc527bd304e12a68.jpg" alt="" className="contactDetailAva" />
-        </div>
-        <div className="contactDetailNameContainer">
-          <h1 style={{ marginTop: 0, marginBottom: 4, fontWeight: 600, textAlign: 'center' }}>Niko Bellic</h1>
-          <p style={{ marginTop: 0, marginBottom: 12, textAlign: 'center' }}>181 Years Old</p>
-        </div>
-        <button className="btnOutWhite" style={{ alignSelf: 'center', marginBottom: 48, paddingLeft: 14, borderRadius: '1.6rem', fontWeight: 600, letterSpacing: 3 }}>Edit</button>
-      </div>
+      <MetaDecorator title="Contact Detail | SimCon" desc="A Page about Detailed information of contact" />
+
+      <ContactDetailContainer
+        id=""
+        firstName=""
+        lastName=""
+        age={0}
+        photo=""
+      />
+
+      <ContactPhoneNumber />
     </div>
   );
 };
--- src/routers/path.ts
+++ src/routers/path.ts
@@ -2,7 +2,7 @@ export const path:any = {
   home: '/',
   detail: '/contact/:contactid',
   add: '/contact/add',
-  edit: '/contact/edit',
+  edit: '/contact/edit/:contactid',
   e404: '/404',
   placeholder: '*',
   placeholderContact: '/contact/*'
